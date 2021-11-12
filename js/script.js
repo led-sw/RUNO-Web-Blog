e64c01609bc19e5f45a13b14ebcd56e9d32a908e
led-sw <lednevsoftware@gmail.com>
2021-11-12 23:06:29 +0300

    com

--- js/script.js
+++ js/script.js
@@ -9,3 +9,12 @@ const swiper = new Swiper('.swiper', {
         delay: 5000,
     },
 });
+const iconMenu = document.querySelector('.menu__icon');
+if (iconMenu) {
+    const menuBody = document.querySelector('.menu__body');
+    iconMenu.addEventListener("click", function(e) {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+}
